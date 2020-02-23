8bdeae39801f5634e8b79e02ab7db86420ff1728
Flermise <735968781@qq.com>
2020-02-24 01:47:52 +0800

    Use interceptor to complete user session injection

--- src/main/java/com/flermise/community/controller/IndexController.java
+++ src/main/java/com/flermise/community/controller/IndexController.java
@@ -1,9 +1,7 @@
 package com.flermise.community.controller;
 
 
-import com.flermise.community.Model.User;
 import com.flermise.community.dto.PaginationDTO;
-import com.flermise.community.mapper.UserMapper;
 import com.flermise.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,36 +9,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
 
     @Autowired
-    private UserMapper userMapper;
-    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(value = "page",defaultValue = "1") Integer page,
-                        @RequestParam(value = "size",defaultValue = "5") Integer size
-    ) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies!=null && cookies.length != 0 )
-        for (Cookie cookie : cookies){
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
-
+                        @RequestParam(value = "size",defaultValue = "5") Integer size) {
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination",pagination);
         return "index";
